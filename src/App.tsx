23a5110fa8b623ddba52b13a50aac151808e273c
Natalya Iskanderova <natalya_iskanderova@epam.com>
2022-12-24 19:20:14 +0100

    Intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+let age: number | string;
+let isStudednt: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+// let printName:Function;
+let printName: (name:string) => number;
+
+// function printName(name:string){
+//   console.log(name)
+// }
+// printName('Iaroslav')
+
+// type Person = {
+//   name: string;
+//   age?: number;
+// } 
+// let person: Person = {
+//   name: 'Iaroslav',
+//   age: 21,
+// }
+
+// let lotsOfPeople: Person[]
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello World
     </div>
   );
 }
